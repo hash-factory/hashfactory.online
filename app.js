42361b9828d17d4776a6fc3d6efcd89b86773199
Interchained <president@worldvaporexpo.com>
2018-09-12 17:46:59 -0400

    update hardfork route

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var blog = require('./routes/blog');
 var docs = require('./routes/docs'); 
 var legal = require('./routes/legal'); 
 var contact = require('./routes/contact');
+// var REQUIREDROOT = require('./path/to/routes/ROUTEjs');
 var hardfork = require('./routes/hardfork'); 
 
 var app = express();
@@ -44,7 +45,8 @@ app.use('/user-guide', docs); // add user-guide route
 app.use('/legal', legal); // add legal route 
 app.use('/tos', legal); // add tos route
 app.use('/privacy', legal); // add privacy route
-app.use('/hardfork', blog); // add hardfork route 
+// app.use('/routeIwantToVisit', REQUIREDROUTE); // something useful...
+app.use('/hardfork', hardfork); // add hardfork route 
 app.use('/', contact);
 
 // catch 404 and forward to error handler
